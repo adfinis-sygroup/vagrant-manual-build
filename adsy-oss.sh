9411b4f410e6d6d2ebd88e66f82f7a7a81170e30
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-07-06 15:25:05 +0200

    Additional packages

--- adsy-oss.sh
+++ adsy-oss.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 ./build.sh \
-	https://github.com/adfinis-sygroup/freeze \
-	https://github.com/adfinis-sygroup/finja \
-	https://github.com/adfinis-sygroup/pyaptly
+    https://github.com/adfinis-sygroup/bs-python-fuzzywuzzy \
+    https://github.com/adfinis-sygroup/bs-python-termcolor \
+    https://github.com/adfinis-sygroup/freeze \
+    https://github.com/adfinis-sygroup/finja \
+    https://github.com/adfinis-sygroup/pyaptly
