f0df375dab61745934162a56210a927a4ea851c7
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-07-06 17:49:08 +0200

    Added binaryornot

--- adsy-oss.sh
+++ adsy-oss.sh
@@ -4,6 +4,7 @@
     https://github.com/adfinis-sygroup/bs-python-click \
     https://github.com/adfinis-sygroup/bs-python-fuzzywuzzy \
     https://github.com/adfinis-sygroup/bs-python-termcolor \
+    https://github.com/adfinis-sygroup/bs-python-binaryornot \
     https://github.com/adfinis-sygroup/freeze \
     https://github.com/adfinis-sygroup/finja \
     https://github.com/adfinis-sygroup/pyaptly
