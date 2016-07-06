f9a430204ec17c7b9ea00ea619791b1a3fa513b6
Jean-Louis Fuchs <ganwell@fangorn.ch>
2016-07-06 16:19:24 +0200

    Halt only current machine

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ done
 for distro in `cat Vagrantfile  | grep define | grep -v -E "\s*#" | cut -d '"' -f 2`; do
     vagrant up $distro && \
         vagrant ssh $distro -c "/vagrant/build.py $distro $C" && \
-        vagrant halt || \
+        vagrant halt $distro || \
         exit 1
 done
 echo All packages successfully built!
